eb991cf58c7d1f9c2330dcfec5f009583195f8d9
Nimisha <nimisha.acharya3009@gmail.com>
2020-09-30 15:18:00 -0400

    Completed desktop and mobile header style.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,8 @@
 import '../styles/styles.css';
+import './modules/MobileMenu';
+import MobileMenu from './modules/MobileMenu';
+
+let mobileMenu = new MobileMenu;
 
 if(module.hot){
     module.hot.accept();
